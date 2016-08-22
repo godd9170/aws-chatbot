343044b6483e4e52f8ade7a757af15cfc7b15733
Henry Goddard <godd9170@mylaurier.ca>
2016-08-22 19:15:29 -0400

    use urlparse to get the query string variables fixes #1

--- api/echo/handler.py
+++ api/echo/handler.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import json
+from urlparse import parse_qs
 import logging
 
 
@@ -9,13 +10,19 @@ log.setLevel(logging.DEBUG)
 
 def handler(event, context):
 	log.debug("Received event {}".format(json.dumps(event)))
+	params = parse_qs(event['body'])
 
-	response = {'success' : 'true'}
-	# if event.get('method') == 'POST':
-	# 	try:
-	# 		response['Success'] = "Nice"
-	# 	except:
-	# 		response['Error'] = "Invalid"
-	# else:
-	# 	response['Error'] = "Invalid"
+	try: 
+		user = params['user_name'][0]
+		command = params['command'][0]
+		channel = params['channel_name'][0]
+		command_text = params['text'][0]
+	except:
+		log.debug("Bad Params")
+
+	response = {}
+	response['response_type'] = "in_channel"
+	response['text'] = "Hello {}".format(user)
+
+
 	return response
